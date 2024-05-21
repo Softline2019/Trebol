58f294d82b7e077acd8d7f50bde947568fa3abbd
YersonG <yersonsalzar11@gmail.com>
2024-05-21 13:26:20 -0500

    Se crea primer crud insert en Comprobantes (Receipts)

---
+++ src/Api/SoftLine.Trebol.WebAPIRest/Controllers/API/ReceiptController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SoftLine.Trebol.Application.Features.Receipts.Commands.CreateReceipts;
+using SoftLine.Trebol.Application.Features.Receipts.Queries.Vms;
+using System.Net;
+
+namespace SoftLine.Trebol.WebAPIRest.Controllers.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReceiptController : ControllerBase
+{
+    private IMediator _mediator;
+
+    public ReceiptController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("create", Name = "CreateReceipt")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ReceiptsVm>> CreateReceipt([FromForm] CreateReceiptsCommand request)
+    {
+        return await _mediator.Send(request);
+    }
+}
---
+++ src/Core/SoftLine.Trebol.Application/Features/Receipts/Commands/CreateReceipts/CreateReceiptsCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using SoftLine.Trebol.Application.Features.Receipts.Queries.Vms;
+
+namespace SoftLine.Trebol.Application.Features.Receipts.Commands.CreateReceipts;
+
+public class CreateReceiptsCommand : IRequest<ReceiptsVm>
+{
+    public int IdCompany { get; set; }
+    public string FNCShort { get; set; }
+    public int fnReceipt { get; set; }
+    public string fcName { get; set; }
+    public int fnConsec { get; set; }
+    public string fcDocRef { get; set; }
+    public bool ReceiptClosing { get; set; }
+
+
+
+}
