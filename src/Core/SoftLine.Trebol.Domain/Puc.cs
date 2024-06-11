4de804f0587857c8cc7531a652de979931f773af
esteban munoz garcia <esteban8356m@gmail.com>
2024-06-11 16:05:52 -0500

    estas son las entidades

---
+++ src/Core/SoftLine.Trebol.Domain/Puc.cs
@@ -0,0 +1,14 @@
+﻿using SoftLine.Trebol.Domain.Common;
+
+namespace SoftLine.Trebol.Domain;
+
+public class Puc : BaseDomainModel
+{
+    public string? NombreCorto { get; set; }
+    public int Cuenta { get; set; }
+    public string? Nombre { get; set; }
+    public int TipoId { get; set; }
+    public Tipo? Tipo { get; set; }
+    public int CodTributario { get; set; }
+    public bool Digitable { get; set; }
+}
---
+++ src/Core/SoftLine.Trebol.Domain/Tipo.cs
@@ -0,0 +1,10 @@
+﻿using SoftLine.Trebol.Domain.Common;
+
+namespace SoftLine.Trebol.Domain
+{
+    public class Tipo : BaseDomainModel
+    {
+        public string? NombreTipo { get; set; }
+        public string? DescripcionTipo { get; set; }
+    }
+}
